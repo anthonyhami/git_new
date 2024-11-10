f033cb1a3aa5c9730da643169f895550fe4c26c8
anthonyhami <66198890+anthonyhami@users.noreply.github.com>
2024-11-10 21:30:10 +0100

    new branch

--- test_pipeline.py
+++ test_pipeline.py
@@ -50,13 +50,7 @@ def test_data_science_pipeline(caplog, dummy_data, dummy_parameters):
         .from_nodes("split_data_node")
         .to_nodes("evaluate_model_node")
     )
-    catalog = DataCatalog()
-    catalog.add_feed_dict(
-        {
-            "model_input_table" : dummy_data,
-            "params:model_options": dummy_parameters["model_options"],
-        }
-    )
+  if bla
 
     caplog.set_level(logging.DEBUG, logger="kedro")
     successful_run_msg = "Pipeline execution completed successfully."
